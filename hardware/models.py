70f52c602a833c836ee5cb91239b210b5fc2e9f4
FidelisMomanyi <fidelismomanyi001@gmail.com>
2025-08-13 12:59:28 +0300

    Added product model and miogrations

--- hardware/models.py
+++ hardware/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=50) #e.g. 'kg', 'piece', 'litre'
+    buying_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_stock = models.IntegerField(default=0)
+    reorder_level = models.IntegerField(default=5) # when to trigger low stock alert
+
+    def __str__(self):
+        return self.name
