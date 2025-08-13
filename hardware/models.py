cfd3f069f87d8402e9d5e2c52b53f9ced7643696
FidelisMomanyi <fidelismomanyi001@gmail.com>
2025-08-14 00:15:25 +0300

    Product model registrations

--- hardware/models.py
+++ hardware/models.py
@@ -7,8 +7,10 @@ class Product(models.Model):
     unit = models.CharField(max_length=50) #e.g. 'kg', 'piece', 'litre'
     buying_price = models.DecimalField(max_digits=10, decimal_places=2)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
-    current_stock = models.IntegerField(default=0)
-    reorder_level = models.IntegerField(default=5) # when to trigger low stock alert
+    current_stock = models.IntegerField(default=0) # will update automatically later
+    reorder_level = models.IntegerField(default=0) # when to trigger low stock alert
 
     def __str__(self):
         return self.name
+
+    from django.db import models
